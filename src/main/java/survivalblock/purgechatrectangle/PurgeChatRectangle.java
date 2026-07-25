bf21798589e3ed7339fdea5625f1579f45ed9377
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-07-24 21:58:16 -0700

    `translatedName` doesn't exist in some versions

--- src/main/java/survivalblock/purgechatrectangle/PurgeChatRectangle.java
+++ src/main/java/survivalblock/purgechatrectangle/PurgeChatRectangle.java
@@ -1,10 +1,13 @@
 package survivalblock.purgechatrectangle;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import survivalblock.purgechatrectangle.config.PurgeChatRectangleConfig;
 
 public class PurgeChatRectangle implements ClientModInitializer {
     public static final String MOD_ID = "purge_chat_rectangle";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
--- src/main/java/survivalblock/purgechatrectangle/config/PurgeChatRectangleMAPICompat.java
+++ src/main/java/survivalblock/purgechatrectangle/config/PurgeChatRectangleMAPICompat.java
@@ -1,9 +1,12 @@
 package survivalblock.purgechatrectangle.config;
 
+import fi.dy.masa.malilib.config.options.ConfigBase;
 import kr1v.malilibApi.MalilibApi;
 import kr1v.malilibApi.annotation.Config;
 import kr1v.malilibApi.config.plus.ConfigBooleanPlus;
 import survivalblock.purgechatrectangle.PurgeChatRectangle;
+
+import java.lang.reflect.Field;
 
 @Config(value = PurgeChatRectangle.MOD_ID, name = "purge_chat_rectangle.tab")
 public class PurgeChatRectangleMAPICompat implements PurgeChatRectangleConfig {
@@ -23,11 +26,35 @@ public class PurgeChatRectangleMAPICompat implements PurgeChatRectangleConfig {
         };
     }
 
-    public static final ConfigBooleanPlus HIDE_INPUT_RECTANGLE = new ConfigBooleanPlus("Hide Input Rectangle");
-    public static final ConfigBooleanPlus HIDE_HISTORY_RECTANGLE = new ConfigBooleanPlus("Hide History Rectangle");
+    public static final ConfigBooleanPlus HIDE_INPUT_RECTANGLE;
+    public static final ConfigBooleanPlus HIDE_HISTORY_RECTANGLE;
 
     static {
-        HIDE_INPUT_RECTANGLE.setTranslatedName(PurgeChatRectangle.MOD_ID + ".hide_input_rectangle");
-        HIDE_HISTORY_RECTANGLE.setTranslatedName(PurgeChatRectangle.MOD_ID + ".hide_history_rectangle");
+        HIDE_INPUT_RECTANGLE = new ConfigBooleanPlus("Hide Input Rectangle");
+        HIDE_HISTORY_RECTANGLE = new ConfigBooleanPlus("Hide History Rectangle");
+
+        String inputTranslated = PurgeChatRectangle.MOD_ID + ".hide_input_rectangle";
+        String historyTranslated = PurgeChatRectangle.MOD_ID + ".hide_history_rectangle";
+
+        Class<?> clazz = ConfigBase.class;
+
+        try {
+            Field translatedName = clazz.getDeclaredField("translatedName");
+            translatedName.setAccessible(true);
+
+            translatedName.set(HIDE_INPUT_RECTANGLE, inputTranslated);
+            translatedName.set(HIDE_HISTORY_RECTANGLE, historyTranslated);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            try {
+                Field prettyName = clazz.getDeclaredField("prettyName");
+                prettyName.setAccessible(true);
+
+                prettyName.set(HIDE_INPUT_RECTANGLE, inputTranslated);
+                prettyName.set(HIDE_HISTORY_RECTANGLE, historyTranslated);
+            } catch (NoSuchFieldException | IllegalAccessException e1) {
+                e1.addSuppressed(e);
+                PurgeChatRectangle.LOGGER.error("Unable to load config translations!", e1);
+            }
+        }
     }
 }
